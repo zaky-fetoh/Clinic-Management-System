982872b76ed5edc7fca6ae6124acaab1bd5907c8
mahmoud zaky <78617936+zaky-fetoh@users.noreply.github.com>
2022-09-11 18:53:03 +0200

    role table is initially developed

---
+++ model/employee_and_schadule/role.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const roleSchema = mongoose.Schema({
+    _id: {
+        type: mongoose.Schema.Types.ObjectId,
+        default: mongoose.Types.ObjectId,
+    },
+    role_name:{
+        type:String, 
+        required: true,
+    }
+})
+
+module.exports = mongoose.model("role", roleSchema)

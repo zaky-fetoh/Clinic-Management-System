f471d79c197b2e36512410bcf82b8ecc92502d72
mahmoud zaky <78617936+zaky-fetoh@users.noreply.github.com>
2022-09-11 15:04:44 +0200

    departmet model initially devoloped

---
+++ model/clinic_and_department/department.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose")
+const clinic_model = require("./clinic")
+const joi = require("joi")
+
+departmentSchema = mongoose.Schema({
+    _id:{
+        type: mongoose.Schema.Types.ObjectId,
+        default: mongoose.Types.ObjectId,
+    },
+    department_name:{
+        type: String, 
+        required: true,
+    },
+    clinic_id:{
+        type:mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref:"clinic", 
+    },
+})
+
+const joiSchema =joi.object({
+    _id: joi.any(), 
+    clinic_id: joi.any(),
+    department_name: joi.string().min(3).max(255).required(),
+});
+
+departmentSchema.pre("save", async function(){
+    const {error}= joiSchema.validate(this._doc)
+    if(error) throw error;
+    const clinic = await clinic_model.findOne({
+        _id: this.clinic_id})
+    if(clinic) throw new Error("This Clinic Does not exist");
+    next();
+});
+
+module.exports = mongoose.model("department",departmentSchema);
+
+

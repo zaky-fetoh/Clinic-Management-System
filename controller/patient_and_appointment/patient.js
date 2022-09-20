8f4d5406944ce66c7c3e18104701849a2fd2e781
mahmoud zaky <78617936+zaky-fetoh@users.noreply.github.com>
2022-09-21 01:39:32 +0200

    CRUD is addded for the patient case collection

--- controller/patient_and_appointment/patient.js
+++ controller/patient_and_appointment/patient.js
@@ -1,38 +1,31 @@
 const patientModel = require("../../model/patient_and_appointment/patient")
 
-    /**
-     * Disc  : 
-     * INPUT : 
-     * OUTPUT: 
-     * ROUTE : 
-     * METHOD: 
-     */
 
-exports.addPatient = async function(req, res, next){
-        /**
-     * Disc  : this method is used to add a patient to the patient collection.
-     * INPUT : post payload for patient document.
-     * OUTPUT: inserted document _id and 0k status flag.
-     * ROUTE : /patient
-     * METHOD: HTTP POST
-     */
+exports.addPatient = async function (req, res, next) {
+    /**
+ * Disc  : this method is used to add a patient to the patient collection.
+ * INPUT : post payload for patient document.
+ * OUTPUT: inserted document _id and 0k status flag.
+ * ROUTE : /patient
+ * METHOD: HTTP POST
+ */
     const body = req.body;
-    try{
-    const pat = await patientModel.create(body)
-    res.status(200).json({
-        ok:true, 
-        message: "patient is added",
-        patient_id: pat._id
-    });
-    }catch(e){
+    try {
+        const pat = await patientModel.create(body)
+        res.status(200).json({
+            ok: true,
+            message: "patient is added",
+            patient_id: pat._id
+        });
+    } catch (e) {
         res.status(500).json({
-            ok:false,
-            message:e.message, 
+            ok: false,
+            message: e.message,
         });
     }
 }
 
-exports.getPatient = async function(req, res, next){
+exports.getPatient = async function (req, res, next) {
     /**
      * Disc  : this method fetch the patient from patient collection 
      * INPUT : patId in the URl of the input.
@@ -41,24 +34,24 @@ exports.getPatient = async function(req, res, next){
      * METHOD: HTTP GET
      */
     const patId = req.params.patId;
-    try{
+    try {
         const doc = patientModel.findOne({
             _id: patId,
-        },{__v:0}); 
+        }, { __v: 0 });
         res.status(200).json({
-            ok:true, 
-            message:"comleted",
-            data:doc,
+            ok: true,
+            message: "comleted",
+            data: doc,
         });
-    }catch(e){
+    } catch (e) {
         res.status(500).json({
-            ok:false, 
+            ok: false,
             message: e.message,
         });
     }
 }
 
-exports.updatePatient = async function( req, res, next){
+exports.updatePatient = async function (req, res, next) {
     /**
      * Disc  : this Method update the patient with agiven patId 
      *         with the payload of the Post method
@@ -66,31 +59,31 @@ exports.updatePatient = async function( req, res, next){
      * OUTPUT: ok status flag
      * ROUTE : /patient/patId
      * METHOD: HTTP PUT
-     */    
+     */
     const patId = req.params.patId;
-    const body =req.body;
-    try{
+    const body = req.body;
+    try {
         const doc = await patientModel.findOne({
-            _id: patId, 
-        }, {__v:0});
-        for(let att in doc){
-            if(att !== "_id" && body[att]) doc[att] = body[att];
+            _id: patId,
+        }, { __v: 0 });
+        for (let att in doc) {
+            if (att !== "_id" && body[att]) doc[att] = body[att];
             doc.save()
         }
         res.status(200).json({
-            ok:true,
-            meassage:"documnet updated",
+            ok: true,
+            meassage: "documnet updated",
         });
-    }catch(e){
+    } catch (e) {
         res.status(500).json({
-            ok:false,
+            ok: false,
             message: e.message,
         })
     }
 }
 
 
-exports.deletePatient = async function( req, res, next){
+exports.deletePatient = async function (req, res, next) {
     /**
      * Disc  : this method deletes the patient documnet of the given PatId 
      * INPUT : patId of url params
@@ -98,20 +91,20 @@ exports.deletePatient = async function( req, res, next){
      * ROUTE : /patient/patId
      * METHOD: HTTP DELETE
      */
-     const patId = req.params.patId;
-     try{
-         const doc = patientModel.deleteOne({
-             _id: patId,
-         }); 
-         res.status(200).json({
-             ok:true, 
-             message:"comleted",
-             result:doc,
-         });
-     }catch(e){
-         res.status(500).json({
-             ok:false, 
-             message: e.message,
-         });
-     }
+    const patId = req.params.patId;
+    try {
+        const doc = patientModel.deleteOne({
+            _id: patId,
+        });
+        res.status(200).json({
+            ok: true,
+            message: "comleted",
+            result: doc,
+        });
+    } catch (e) {
+        res.status(500).json({
+            ok: false,
+            message: e.message,
+        });
+    }
 }
---
+++ controller/patient_and_appointment/patient_case.js
@@ -0,0 +1,114 @@
+const patientCaseModel = require("../../model/patient_and_appointment/patient_case");
+
+
+
+exports.addPatientCase = async function (req, res, next) {
+    /**
+     * Disc  : this method addes a patientCase doc in patient case collection
+     * INPUT : payload body of the POST method
+     * OUTPUT: _id of the inserted patient Case Object
+     * ROUTE : /patient/patId/case/
+     * METHOD: HTTP POST
+     */
+    const body = req.body;
+    const patId =  req.params.patId
+    try {
+        Object.assign(body, {
+            patient_id: patId
+        });
+        const pat = await patientCaseModel.create(body)
+        res.status(200).json({
+            ok: true,
+            message: "patient case is added",
+            patient_id: pat._id
+        });
+    } catch (e) {
+        res.status(500).json({
+            ok: false,
+            message: e.message,
+        });
+    }
+}
+
+exports.getPatient = async function (req, res, next) {
+    /**
+     * Disc  : retreve the patient case doc form the patientCase model
+     * INPUT : patientCaseId
+     * OUTPUT: the required document
+     * ROUTE : /patient-case/pcId
+     * METHOD: HTTP GET
+     */
+    const pcId = req.params.pcId;
+    try {
+        const doc = patientCaseModel.findOne({
+            _id: pcId,
+        }, { __v: 0 });
+        res.status(200).json({
+            ok: true,
+            message: "comleted",
+            data: doc,
+        });
+    } catch (e) {
+        res.status(500).json({
+            ok: false,
+            message: e.message,
+        });
+    }
+}
+
+exports.updatePatient = async function (req, res, next) {
+    /**
+     * Disc  : update patient case with the PostPayload
+     * INPUT : the patientcaseId in params and the payload of to update with
+     * OUTPUT: ok status
+     * ROUTE : /patient-case/pcId
+     * METHOD: HTTP Put
+     */
+    const pcId = req.params.pcId;
+    const body = req.body;
+    try {
+        const doc = await patientCaseModel.findOne({
+            _id: pcId,
+        }, { __v: 0 });
+        for (let att in doc) {
+            if (att !== "_id" && body[att]) doc[att] = body[att];
+            doc.save()
+        }
+        res.status(200).json({
+            ok: true,
+            meassage: "documnet updated",
+        });
+    } catch (e) {
+        res.status(500).json({
+            ok: false,
+            message: e.message,
+        })
+    }
+}
+
+
+exports.deletePatient = async function (req, res, next) {
+    /**
+     * Disc  : delete the patientcase method from the Pc collection
+     * INPUT : the patientCaseId in the url params
+     * OUTPUT: ok status and the total number of the deleted status
+     * ROUTE : /patient-case/pcId
+     * METHOD: HTTP DELETE 
+     */
+    const pcId = req.params.pcId;
+    try {
+        const doc = patientCaseModel.deleteOne({
+            _id: pcId,
+        });
+        res.status(200).json({
+            ok: true,
+            message: "comleted",
+            result: doc,
+        });
+    } catch (e) {
+        res.status(500).json({
+            ok: false,
+            message: e.message,
+        });
+    }
+}

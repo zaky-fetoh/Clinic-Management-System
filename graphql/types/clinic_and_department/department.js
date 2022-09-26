684b8dbce3b3b7da81b336f81081475f02fc0e64
mahmoud zaky <78617936+zaky-fetoh@users.noreply.github.com>
2022-09-26 17:00:13 +0200

    department type is added

---
+++ graphql/types/clinic_and_department/department.js
@@ -0,0 +1,17 @@
+const gql = require("graphql");
+
+exports.DepartmentType = gql.GraphQLObjectType({
+    name: "department",
+    description: "department collection type",
+    fields: {
+        _id: {
+            type: gql.GraphQLString,
+        },
+        department_name: {
+            type: gql.GraphQLString,
+        },
+        clinic_id: {
+            type: gql.GraphQLString,
+        },
+    }
+})

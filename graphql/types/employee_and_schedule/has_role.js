0c2184bd47d0fc7c699cf97bc84e0be0d78e2288
mahmoud zaky <78617936+zaky-fetoh@users.noreply.github.com>
2022-09-26 19:19:19 +0200

    GraphalHasRole is added

--- graphql/types/employee_and_schedule/has_role.js
+++ graphql/types/employee_and_schedule/has_role.js
@@ -1,29 +1,28 @@
+const GraphQLDate = require("../DateType");
 const gql = require("graphql");
 
+
 exports.HasRoleType = new gql.GraphQLObjectType({
-    name:"HasRoleType", 
-    description:"has-role collection Type",
-    fields:{
+    name: "HasRoleType",
+    description: "has-role collection Type",
+    fields: {
         _id: {
             type: gql.GraphQLString,
-          },
-          employee_id: {
+        },
+        employee_id: {
             type: gql.GraphQLString,
-          },
-          role_id: {
+        },
+        role_id: {
             type: gql.GraphQLString,
-          },
-          time_from: {
-            type:  gql.GraphQLString,
-            resolve:(parent)=>{
-                return parent
-            }
-          },
-          time_to: {
-            type:  gql.GraphQLString,
-          },
-          is_active: {
+        },
+        time_from: {
+            type: GraphQLDate,
+        },
+        time_to: {
+            type: GraphQLDate,
+        },
+        is_active: {
             type: gql.GraphQLBoolean,
-          },
+        },
     }
 })

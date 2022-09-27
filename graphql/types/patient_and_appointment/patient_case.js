8d4b7bb1bde85162cfc407a5465f2f6f19bd4ff9
mahmoud zaky <78617936+zaky-fetoh@users.noreply.github.com>
2022-09-27 13:36:56 +0200

    patient Case graphqltype is added

--- graphql/types/patient_and_appointment/patient_case.js
+++ graphql/types/patient_and_appointment/patient_case.js
@@ -1,3 +1,31 @@
 const GraphQLObjectId = require("../ObjectIdType");
 const GraphQLDate = require("../DateType");
 const gql = require("graphql");
+
+exports.PatientCase = new gql.GraphQLObjectType({
+    name:"patient_case", 
+    description:"patient Case type ofthe corresponding collection",
+    fields:{
+        _id: {
+            type: GraphQLObjectId,
+          },
+          patient_id: {
+            type: GraphQLObjectId,
+          },
+          start_time: {
+            type: GraphQLDate,
+          },
+          end_time: {
+            type: GraphQLDate,
+          },
+          in_progress: {
+            type: gql.GraphQLBoolean,
+          },
+          total_cost: {
+            type: gql.GraphQLFloat,
+          },
+          amount_paid: {
+            type: gql.GraphQLFloat,
+          },
+    }
+})
--- graphql/types/patient_and_appointment/status_history.js
+++ graphql/types/patient_and_appointment/status_history.js
@@ -1,3 +1,11 @@
 const GraphQLObjectId = require("../ObjectIdType");
 const GraphQLDate = require("../DateType");
 const gql = require("graphql");
+
+exports. = new gql.GraphQLObjectType({
+    name:"", 
+    description:"",
+    fields:{
+
+    }
+})

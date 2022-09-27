c6814afb66647c5dae0a5c6833e85b1fec7a06f1
mahmoud zaky <78617936+zaky-fetoh@users.noreply.github.com>
2022-09-27 13:42:30 +0200

    Status History gqltype is added

--- graphql/types/patient_and_appointment/status_history.js
+++ graphql/types/patient_and_appointment/status_history.js
@@ -2,10 +2,24 @@ const GraphQLObjectId = require("../ObjectIdType");
 const GraphQLDate = require("../DateType");
 const gql = require("graphql");
 
-exports. = new gql.GraphQLObjectType({
-    name:"", 
-    description:"",
+exports.StatusHistoryType = new gql.GraphQLObjectType({
+    name:"StatusHistoryType", 
+    description:"status History Type",
     fields:{
-
+        _id: {
+            type: GraphQLObjectId,
+          },
+          appointment_id: {
+            type: GraphQLObjectId,
+          },
+          appointment_status_id: {
+            type: GraphQLObjectId,
+          },
+          status_time: {
+            type: GraphQLDate,
+          },
+          details: {
+            type: gql.GraphQLString,
+          },
     }
 })

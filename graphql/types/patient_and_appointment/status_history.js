111e26a51c732b7b83f586a5d9a633d5dc3f2923
Zaky fetoh <zaky.fetoh@gmail.com>
2022-10-14 00:23:49 +0200

    status hist is addede

--- graphql/types/patient_and_appointment/status_history.js
+++ graphql/types/patient_and_appointment/status_history.js
@@ -6,20 +6,34 @@ exports.StatusHistoryType = new gql.GraphQLObjectType({
     name: "StatusHistoryType",
     description: "status History Type",
     fields: ()=>({
-        _id: {
+        _id: {type: GraphQLObjectId,},
+        appointment_id: {
         type: GraphQLObjectId,
         },
-        appointment_id: {
-            type: GraphQLObjectId,
-        },
         appointment_status_id: {
-            type: GraphQLObjectId,
+        type: GraphQLObjectId,
         },
         status_time: {
-            type: GraphQLDate,
+        type: GraphQLDate,
         },
         details: {
-            type: gql.GraphQLString,
+        type: gql.GraphQLString,
         },
+
+        get_appointment:{
+            type:gql.require("../index").AppointmentType,
+            resolve:async(parent,_,{appointmentModel})=>{
+                return await appointmentModel.find({
+                    _id:parent.appointment_id,
+                })
+            }},
+        get_appointment_status:{
+                type: require("../index").AppointmentStatusType,
+                resolve:async(parent,_, {appointmentStatusModel})=>{
+                  return await appointmentStatusModel.findOne({
+                    _id: parent.appointment_status_id
+                  })}
+              },
+
     })
 })

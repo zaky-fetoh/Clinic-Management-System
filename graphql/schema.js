24eb4106fde26ac1fe7e5bca288f4c95204b1efb
Zaky fetoh <zaky.fetoh@gmail.com>
2022-10-15 14:06:01 +0200

    root quary is updated

--- graphql/schema.js
+++ graphql/schema.js
@@ -1,6 +1,6 @@
 const  gql = require("graphql")
 
-const rootQuery = require("./query/root_query")
+const rootQuery = require("./types/root_query")
 const rootMutation = require("./mutation/root_mutation");
 
 
--- graphql/types/root_query.js
+++ graphql/types/root_query.js
@@ -1,16 +1,13 @@
 const gql = require("graphql");
-
-const types = require("../types/index");
-
-const ClinicModel =require("../../model/clinic_and_department/clinic");
+const types = require("./index");
 
 module.exports = new gql.GraphQLObjectType({
     name: "root_query",
     fields: {
         getAllClinics: {
             type: gql.GraphQLList(types.ClinicType),
-            resolve: async () => {
-                return await ClinicModel.find({})
+            resolve: async (parent, args, {clinicModel}) => {
+                return await clinicModel.find({})
             }
         }
     }

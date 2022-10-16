fcded7e9a9e0a98224a51d27d90269beae4aaec4
Zaky fetoh <zaky.fetoh@gmail.com>
2022-10-16 20:19:44 +0200

    status History gqltype is refactored

--- graphql/types/patient_and_appointment/status_history.js
+++ graphql/types/patient_and_appointment/status_history.js
@@ -2,23 +2,27 @@ const GraphQLObjectId = require("../ObjectIdType");
 const GraphQLDate = require("../DateType");
 const gql = require("graphql");
 
+exports.StatusHistoryFields = {
+    _id: {type: GraphQLObjectId,},
+    appointment_id: {
+    type: GraphQLObjectId,
+    },
+    appointment_status_id: {
+    type: GraphQLObjectId,
+    },
+    status_time: {
+    type: GraphQLDate,
+    },
+    details: {
+    type: gql.GraphQLString,
+    },
+
+}
+
 exports.StatusHistoryType = new gql.GraphQLObjectType({
     name: "StatusHistoryType",
     description: "status History Type",
-    fields: ()=>({
-        _id: {type: GraphQLObjectId,},
-        appointment_id: {
-        type: GraphQLObjectId,
-        },
-        appointment_status_id: {
-        type: GraphQLObjectId,
-        },
-        status_time: {
-        type: GraphQLDate,
-        },
-        details: {
-        type: gql.GraphQLString,
-        },
+    fields: ()=>Object.assign({
 
         get_appointment:{
             type:require("../index").AppointmentType,
@@ -32,8 +36,6 @@ exports.StatusHistoryType = new gql.GraphQLObjectType({
                 resolve:async(parent,_, {appointmentStatusModel})=>{
                   return await appointmentStatusModel.findOne({
                     _id: parent.appointment_status_id
-                  })}
-              },
-
-    })
+                  })}},
+    }, exports.StatusHistoryFields)
 })

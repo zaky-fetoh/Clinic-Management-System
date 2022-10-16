fe478d577cdef8c0a32c27bcb536f8cbb048e367
Zaky fetoh <zaky.fetoh@gmail.com>
2022-10-16 19:50:49 +0200

    clinic gqltype refactored

--- graphql/types/clinic_and_department/clinic.js
+++ graphql/types/clinic_and_department/clinic.js
@@ -2,32 +2,35 @@ const GraphQLObjectId = require("../ObjectIdType");
 const gql = require("graphql")
 
 
+exports.ClinicFields = {
+    _id: {
+    type: GraphQLObjectId,
+    },
+    clinic_name: {
+        type: gql.GraphQLString,
+    },
+    address: {
+        type: gql.GraphQLString,
+    },
+    details: {
+        type: gql.GraphQLString,
+    },
+}
+
 exports.ClinicType = new gql.GraphQLObjectType({
     name: "clinic",
     description: "type for the clinic collication",
-    fields: ()=>({
-        _id: {
-        type: GraphQLObjectId,
-        },
-        clinic_name: {
-            type: gql.GraphQLString,
-        },
-        address: {
-            type: gql.GraphQLString,
-        },
-        details: {
-            type: gql.GraphQLString,
-        },
+    fields: ()=>Object.assign({
         ///Queries
-        get_departments:{
+        get_departments: {
             type: gql.GraphQLList(
-            require("../index").DepartmentType
+                require("../index").DepartmentType
             ),
-            resolve:async(parent,_,{departmentModel})=>{
+            resolve: async (parent, _, { departmentModel }) => {
                 return await departmentModel.find({
                     clinic_id: parent._id
                 })
             },
         }
-    }),
+    },exports.ClinicFields),
 });

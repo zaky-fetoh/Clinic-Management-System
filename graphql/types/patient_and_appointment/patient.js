657dba3ebf7322b8d22d70871a92561627903a10
Zaky fetoh <zaky.fetoh@gmail.com>
2022-10-16 20:14:45 +0200

    patient gqltype is refactored

--- graphql/types/patient_and_appointment/patient.js
+++ graphql/types/patient_and_appointment/patient.js
@@ -1,11 +1,8 @@
 const GraphQLObjectId = require("../ObjectIdType");
 const gql = require("graphql");
 
-exports.PatientType = new gql.GraphQLObjectType({
-    name:"patient", 
-    description:"patient type of patient collection",
-    fields:()=>({
-        _id:{
+exports.PatientFields= {
+    _id:{
         type: GraphQLObjectId,
         }, 
         first_name:{
@@ -14,6 +11,12 @@ exports.PatientType = new gql.GraphQLObjectType({
         last_name:{ 
             type: gql.GraphQLString, 
         },
+}
+
+exports.PatientType = new gql.GraphQLObjectType({
+    name:"patient", 
+    description:"patient type of patient collection",
+    fields:()=>Object.assign({
         get_patient_cases:{
             type:gql.GraphQLList(
             require("../index").PatientCaseType),
@@ -23,5 +26,5 @@ exports.PatientType = new gql.GraphQLObjectType({
                 });
             }
         }
-    })
+    }, exports.PatientFields)
 })
